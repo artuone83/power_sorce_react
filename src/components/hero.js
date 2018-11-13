13cc6282f3c68bb85aa33dbd91a7c099711aadf1
Artur Wozniak <wozniaka83@gmail.com>
2018-11-13 15:51:15 +0100

    img onClick and axios power val

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,24 +1,70 @@
 import React, {Component} from 'react';
 import TotalPower from './total_power';
+import axios from 'axios';
+const apiURL = 'https://challenge.codetain.com/api/v1/charging_status'; 
 class Hero extends Component {
-  render() {
-    return (
-      <section className="total-power">
-        <div className="container">
-          <TotalPower totalPower='0'/>
-          <div className="power-consumption">
-          <figure className="millennium-falcon">
-            <img src="./img/m_falcon.png" alt="Millennium Falcon" />
-            <figcaption className="falcon-power">0 kW</figcaption>
-          </figure>
-          <figure className="lightsaber">
-            <img src="./img/lightsaber.png" alt="Lightsaber" />
-            <figcaption className="lightsaber-power">0 kW</figcaption>
-          </figure>
-          </div>
-        </div>
-      </section>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			apiURL: 'https://challenge.codetain.com/api/v1/charging_status',
+			falconURL: 'https://challenge.codetain.com/api/v1/charging_status?priority=falcon',
+			falconURL:'https://challenge.codetain.com/api/v1/charging_status?priority=falcon',
+			totalPower: 0,
+			falconPower: 0,
+			saberPower: 0
+		}
+		this.setFalconPower = this.setFalconPower.bind(this);
+		this.setSaberPower = this.setSaberPower.bind(this);
+	}
+	componentDidMount() {
+		axios.get(apiURL)
+			.then(res => {
+				this.setState({
+					falconPower: res.data.charging_status.falcon,
+					saberPower: res.data.charging_status.lightsaber,
+					totalPower: this.state.falconPower + this.state.saberPower
+				});
+				console.log(res.data.charging_status.falcon);
+				console.log(res.data.charging_status.lightsaber);
+				console.log(this.state.totalPower);
+			})
+	} 
+	setFalconPower() {
+		this.setState({
+			falconPower: Math.floor(Math.random() * 8),
+			totalPower: this.state.falconPower,		
+		});				
+	}
+	setSaberPower() {
+		this.setState({
+			saberPower: Math.floor(Math.random() * 8),
+			//totalPower: this.state.saberPower,		
+		});
+	}
+	render() {
+		return (
+			<section className="total-power">
+				<div className="container">
+					<TotalPower totalPower={this.state.totalPower}/>
+					<div className="power-consumption">
+					<figure className="millennium-falcon">
+						<img
+							src="./img/m_falcon.png"
+							alt="Millennium Falcon"
+							onClick={this.setFalconPower} />
+						<figcaption className="falcon-power">{this.state.falconPower} kW</figcaption>
+					</figure>
+					<figure className="lightsaber">
+						<img 
+							src="./img/lightsaber.png"
+							alt="Lightsaber"
+							onClick={this.setSaberPower} />
+						<figcaption className="lightsaber-power">{this.state.saberPower} kW</figcaption>
+					</figure>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 export default Hero;

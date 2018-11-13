96dd046855c774f8a0f1baae129ca6ff193c65eb
Artur Wozniak <wozniaka83@gmail.com>
2018-11-13 11:46:33 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/status_style.scss';
+ReactDOM.render(
+  <h1>Welcom on React side</h1>,
+  document.querySelector('.app')
+);

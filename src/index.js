8a901baad084762a20c977ddca26e03bc6857b18
Artur Wozniak <wozniaka83@gmail.com>
2018-11-13 14:04:49 +0100

    app component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './scss/status_style.scss';
 import img from './img/power2.jpg';
+import PageHeader from './components/page_header';
+import Hero from './components/hero';
+import Footer from './components/footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PageHeader />
+        <Hero />
+        <Footer/>
+      </div>
+    );
+  }
+}
 ReactDOM.render(
-  <h1>Welcom on React side</h1>,
+  <App />,
   document.querySelector('.app')
 );

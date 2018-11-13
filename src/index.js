3f7db808a82690a5e2ca572fe6b792e6cdb9c6d8
Artur Wozniak <wozniaka83@gmail.com>
2018-11-13 12:09:19 +0100

    file- loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/status_style.scss';
+import img from './img/power2.jpg';
 ReactDOM.render(
   <h1>Welcom on React side</h1>,
   document.querySelector('.app')
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,17 @@ module.exports = {
           "css-loader",
           "sass-loader"
         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img/'
+            }
+          }
+        ]
       }
     ]
   },

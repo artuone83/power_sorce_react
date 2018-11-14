d6b9d8f9af48d62eb0ab3f8ee07473c17f306f22
Artur Wozniak <wozniaka83@gmail.com>
2018-11-14 11:48:32 +0100

    const, addClass state, getPowerVal - interval, toggle class on click

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,45 +1,54 @@
 import React, {Component} from 'react';
 import TotalPower from './total_power';
 import axios from 'axios';
-const apiURL = 'https://challenge.codetain.com/api/v1/charging_status'; 
+
+const falconURL = 'https://challenge.codetain.com/api/v1/charging_status?priority=falcon';
+const saberURL = 'https://challenge.codetain.com/api/v1/charging_status?priority=lightsaber';
 class Hero extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			apiURL: 'https://challenge.codetain.com/api/v1/charging_status',
-			falconURL: 'https://challenge.codetain.com/api/v1/charging_status?priority=falcon',
-			falconURL:'https://challenge.codetain.com/api/v1/charging_status?priority=falcon',
-			totalPower: 0,
+			apiURL: 'https://challenge.codetain.com/api/v1/charging_status',					
 			falconPower: 0,
-			saberPower: 0
+			saberPower: 0,
+			addClassFalcon: false,
+			addClassSaber: false
 		}
 		this.setFalconPower = this.setFalconPower.bind(this);
 		this.setSaberPower = this.setSaberPower.bind(this);
+		this.getPowerVal = this.getPowerVal.bind(this)
 	}
+
+	getPowerVal() {
+		axios.get(this.state.apiURL)
+			.then(res => {
+				this.setState({
+					falconPower: res.data.charging_status.falcon,
+					saberPower: res.data.charging_status.lightsaber					
+				});
+				this.state.falconPower != 0 ? this.setState({addClassFalcon: true}) : this.setState({addClassFalcon: false});
+				this.state.saberPower != 0 ? this.setState({addClassSaber: true}) : this.setState({addClassSaber: false});
+			})
+	}
+
 	componentDidMount() {		
-			axios.get(apiURL)
-				.then(res => {
-					this.setState({
-						falconPower: res.data.charging_status.falcon,
-						saberPower: res.data.charging_status.lightsaber,
-						totalPower: this.state.falconPower + this.state.saberPower
-					});
-					console.log(res.data.charging_status.falcon);
-					console.log(res.data.charging_status.lightsaber);
-					console.log(this.state.totalPower);
-				})
+			setInterval(this.getPowerVal, 3000);
 		}
 	setFalconPower() {
 		this.setState({
 			falconPower: Math.floor(Math.random() * 8),
-			totalPower: this.state.falconPower,		
-		});				
+			apiURL: falconURL,
+			addClassFalcon: !this.state.addClassFalcon				
+		});
+					
 	}
 	setSaberPower() {
 		this.setState({
 			saberPower: Math.floor(Math.random() * 8),
-			//totalPower: this.state.saberPower,		
+			apiURL: saberURL,
+			addClassSaber: !this.state.addClassSaber					
 		});
+		
 	}
 	render() {
 		return (
@@ -51,6 +60,7 @@ class Hero extends Component {
 						<img
 							src="./img/m_falcon.png"
 							alt="Millennium Falcon"
+							className= {this.state.addClassFalcon ? 'green-border' : 'gray-border'}
 							onClick={this.setFalconPower} />
 						<figcaption className="falcon-power">{this.state.falconPower} kW</figcaption>
 					</figure>
@@ -58,6 +68,7 @@ class Hero extends Component {
 						<img 
 							src="./img/lightsaber.png"
 							alt="Lightsaber"
+							className= {this.state.addClassSaber ? 'green-border' : 'gray-border'}
 							onClick={this.setSaberPower} />
 						<figcaption className="lightsaber-power">{this.state.saberPower} kW</figcaption>
 					</figure>
